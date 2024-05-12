545f42fd4c7aa8daccaec3e3b4389c5d60ad04c8
Wai Ho Choy <choywaiho1@gmail.com>
2024-05-12 09:46:39 -0700

    ci: added rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,32 @@
+
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const packageJson = require("./package.json");
+
+export default {
+  input: "src/index.ts",
+  // https://nodejs.org/api/packages.html#dual-commonjses-module-packages
+  output: [
+    {
+      file: packageJson.exports['.'].require,
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: packageJson.exports['.'].import,
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript(),
+    json()
+  ]
+};
